b006f90630c963a6ce80f4981a28fec1552d76aa
Ashraf Anwar <ashrafanwar@Spartas-MacBook-Pro.local>
2018-01-29 01:35:45 +0000

    Added routing code

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'; // give access to route parameter
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { 
+    this.route.params.subscribe((res) => console.dir(res));
+  }
 
   ngOnInit() {
   }
 
+  sendMeHome() {
+    this.router.navigate(['']);
+  }
 }
